206f06f097723ccf4cb5d89bed34d13de9f8cff6
Alex <ph0t0nix@ph0t0nixs-MacBook-Pro.local>
2019-10-27 21:31:04 -0500

    Updates

---
+++ precache-manifest.c1d6fc7ec231e5ebac4bb0b58005ffff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623a101cebe4d521569d541a6e4ee5b4",
+    "url": "/starwars-api/index.html"
+  },
+  {
+    "revision": "be20aca9c818e1e1469c",
+    "url": "/starwars-api/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "784bb112f0c257c2c474",
+    "url": "/starwars-api/static/css/main.1733b4c4.chunk.css"
+  },
+  {
+    "revision": "be20aca9c818e1e1469c",
+    "url": "/starwars-api/static/js/2.220cd4b6.chunk.js"
+  },
+  {
+    "revision": "784bb112f0c257c2c474",
+    "url": "/starwars-api/static/js/main.d8b38705.chunk.js"
+  },
+  {
+    "revision": "60fccbff8b8368759afc",
+    "url": "/starwars-api/static/js/runtime~main.060f48ba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwars-api/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "472327635b273f9f2cfa1623c11661e6",
+    "url": "/starwars-api/static/media/darth-vader.47232763.png"
+  }
+]);
